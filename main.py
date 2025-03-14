c73796713f56fb7bc26d8c12786c25aa5899b78b
Kalle Korhonen <kalle.korhonen@kone.com>
2025-03-14 14:58:33 +0200

    Add argparse

--- main.py
+++ main.py
@@ -1,15 +1,44 @@
+import argparse
 from dnsquery import DNSResolver
 from takeover import brute_force
 from logger_config import setup_logging
 
 logger = setup_logging()
 
+parser = argparse.ArgumentParser(
+    description="Test AWS Route 53 hosted zone NS assignments."
+)
+parser.add_argument(
+    "domain",
+    type=str,
+    help="The domain name for the hosted zone",
+)
+parser.add_argument(
+    "--max_attempts",
+    type=int,
+    default=1000,
+    help="Maximum attempts to try to perform takeover.",
+)
+parser.add_argument(
+    "-s",
+    "--single",
+    action="store_true",
+    help="Only try to takeover one of the nameservers.",
+)
+parser.add_argument(
+    "-f", "--force", action="store_true", help="Don't ask for confirmations."
+)
+
+args = parser.parse_args()
+
+
+
 def main():
     dns_resolver = DNSResolver()
     #manager = Route53Manager()
-    name_servers = dns_resolver.get_aws_ns_servers("example.com")
+    name_servers = dns_resolver.get_aws_ns_servers(args.domain)
     logger.info(name_servers)
-    result = brute_force("example.com", name_servers, 1000)
+    result = brute_force(args.domain, name_servers, args.max_attempts)
     print(result)
 
 
